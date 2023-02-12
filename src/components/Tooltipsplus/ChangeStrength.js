84d3be230c94dec942a48bba73b3ec8d037b3777
Hexparty <primparty.reg@gmail.com>
2023-02-12 21:23:27 +0300

    Old Folders deleted

--- src/components/Tooltipsplus/ChangeStrength.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "@nextui-org/react";
-
-export const ChangeStrength = () => {
-    return (
-        <Text>Choose how much you want your image to transform!</Text>
-    )
-}
--- src/components/Tooltipsplus/ChooseStyle.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "@nextui-org/react";
-
-export const ChooseStyle = () => {
-    return (
-        <Text>Choose the style you want your image to transform to</Text>
-    )
-}

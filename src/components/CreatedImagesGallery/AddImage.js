8d56fc1b347bd9bf81e2e91d019c0246aa2c7864
Hexparty <primparty.reg@gmail.com>
2023-03-23 16:50:07 +0300

    Resolved: 1. Scroll to top on uploading Image

--- src/components/CreatedImagesGallery/AddImage.js
+++ src/components/CreatedImagesGallery/AddImage.js
@@ -1,19 +1,26 @@
+import { useRef, useEffect } from 'react';
 import { AddImageIcon } from '../ButtonsAndIcons/AddImageIcon';
 
 export default function AddImage({ onFileChange, selectedFileUrl, fileName }) {
-    return (
-        <div className="img-wrap empty">
-            <label htmlFor="uploadimage">
-                <AddImageIcon />
-                <input
-                    type="file"
-                    onChange={(e) => onFileChange(e)}
-                    title={fileName}
-                    name="uploadimage"
-                    accept="image/png, image/gif, image/jpeg"
-                />
-            </label>
-            <img className="loaded-image" alt="" src={selectedFileUrl} />
-        </div>
-    )
+    const scollToRef = useRef();
+    useEffect(() => {
+        scollToRef.current.scrollIntoView({ behavior: "smooth" });
+    }, []);
+    
+
+return (
+    <div className="img-wrap empty" ref={scollToRef}>
+        <label htmlFor="uploadimage">
+            <AddImageIcon />
+            <input
+                type="file"
+                onChange={(e) => onFileChange(e)}
+                title={fileName}
+                name="uploadimage"
+                accept="image/png, image/gif, image/jpeg"
+            />
+        </label>
+        <img className="loaded-image" alt="" src={selectedFileUrl} />
+    </div>
+)
 }

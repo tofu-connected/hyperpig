5d76fd603fae94592daf2e8953b5b9bdd1b6db3b
tofu <tofu.connected@gmail.com>
2023-07-22 05:35:58 +0900

    styles on front end

--- src/hyperlib.js
+++ src/hyperlib.js
@@ -11,37 +11,47 @@ export async function searchMovies(title) {
   return data.Search;
 }
 
+export async function getStyles() {
+  // TODO: request to backpig
+    return  [
+      {
+        id: "1", 
+        name: "CyberSwine",
+        prompt: "cyberpunk, dark city, masterpiece, best quality", 
+        negative_prompt:"bad quality, worst quality, mutation, extra limbs",
+        image_url:"https://m.media-amazon.com/images/M/MV5BMTc5NDQzOTc2N15BMl5BanBnXkFtZTcwNDI5MzI4Nw@@._V1_SX300.jpg"
+      },
+      { 
+        id: "2", 
+        name: "Pigruto",
+        prompt: "naruto, anime, from naruto, fine artwork, masterpiece, best quality", 
+        negative_prompt:"bad quality, worst quality, mutation, extra limbs",
+        image_url:"https://m.media-amazon.com/images/M/MV5BZWEzMzVkMmItMWQ4My00ODIzLTk2ZDMtN2I5NDY1ZjBmOTVkXkEyXkFqcGdeQXVyMTE0MzY0NjE1._V1_SX300.jpg"
+      }
+      ]
+}
+
+export async function updateCards(styles, setCards) {
+  setCards( 
+    styles.map((style) => {
+      return {
+          id: style.id,
+          name: style.name,
+          prompt: style.prompt,
+          image_url: style.image_url,
+        }
+      }));
+}
+
 export async function getBlob(url) {
   const response = await fetch(url);
   return response.blob();
 }
 
-/* export function genFish() {
-  function importAll(r) {
-    let images = {};
-    r.keys().map((item) => (images[item.replace('./', '')] = r(item)));
-    return images;
-  }
-  const images = importAll(require.context("./img", false, /\.(png|jpe?g|svg)$/));
-
-  return [{
-    prompt: "military angel relaxing in a bingbag",
-    src: images["6.png"],
-    orientation: "vertical"
-  },
-  {
-    prompt: "boom glashing eyes colorful head",
-    src: images["13.jpg"],
-    orientation: "notvertical"
-  }]
-} */
-
-export async function runInference(params) {
-
+export async function img2img(params) {
+  // TODO: backpig.su/api/img2img?image&params&style_id
   const serverUrl = `http://125.199.220.228:7860/sdapi/v1/img2img`;
-
-  const inputBlob = await getBlob(params.img_url);
-
+  const init_image = await blobToBase64( await fetch(params.img_url).then((res) => res.blob()) );
   const response = await fetch(serverUrl, {
     method: 'POST',
     headers: {
@@ -49,11 +59,12 @@ export async function runInference(params) {
       'accept': 'application/json'
     },
     body: JSON.stringify({
-      prompt: "",
-      init_images: [await blobToBase64(inputBlob)],
-      sampler_name: "Euler a",
+      prompt: params.style.prompt,
+      negative_prompt: params.style.negative_prompt,
+      init_images: [init_image],
+      sampler_name: "DPM++ 2S a Karras",
       steps: 20,
-      cfg_scale: 0,
+      cfg_scale: 7,
       denoising_strength: params.strength / 100,
     })
   });
